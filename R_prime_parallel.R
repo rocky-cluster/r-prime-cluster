d972ac1e1d977fc588c5a008dad37a009f924432
GrahamDB <g.derryberry@gmail.com>
2023-04-10 13:12:53 -0400

    Create R_prime_parallel.R

---
+++ R_prime_parallel.R
@@ -0,0 +1,16 @@
+library(foreach)
+library(doParallel)
+
+registerDoParallel(40)
+
+known_primes <- 
+  c(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31)
+
+search_space <- 34:999
+
+found_primes <- foreach(check_i= search_space, .combine=c() ) %dopar% {
+  if(any((check_i %% known_primes )==0 ) return(list())
+  return(list(check_i))
+}
+
+write.table(sort(c(known_primes, found_primes)), file="R_prime_parallel.txt")
