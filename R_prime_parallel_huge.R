200d50cc7a8724e2818f0ea750821752f5c088a3
Graham Derryberry <g.derryberry@gmail.com>
2023-04-10 14:14:08 -0400

    Reduce checks by 60%

--- R_prime_parallel_huge.R
+++ R_prime_parallel_huge.R
@@ -19,11 +19,11 @@ found_primes <- foreach(check_i= search_space, .combine=c ) %dopar% {
 
 # primes less than 10^6
 known_primes <- c(known_primes, found_primes)
-
+search_space_3 <- rep(0:99, each=4)*10 + rep(c(1,3,7,9), 100)
 
 found_primes_6 <- foreach(search_i= icount(1e3), .combine=c ) %dopar% {
   
-  res <- foreach(check_i= search_i*1e3 +(0:999), .combine=c ) %do% {
+  res <- foreach(check_i= search_i*1e3 +search_space_3, .combine=c ) %do% {
     if(any((check_i %% known_primes )==0 ) ) return(integer())
     return(c(check_i))
   }
