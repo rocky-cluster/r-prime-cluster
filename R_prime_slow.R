2fe6acea83f7dd50ec5e81588a582277fbfa7d4e
GrahamDB <g.derryberry@gmail.com>
2023-04-20 14:31:52 -0400

    Update R_prime_slow.R

--- R_prime_slow.R
+++ R_prime_slow.R
@@ -9,6 +9,7 @@ search_space <- 10:99
 found_primes <- foreach(check_i= search_space, .combine=c ) %do% {
   Sys.sleep(1)
   if(any((check_i %% known_primes )==0 ) ) return(numeric())
+  cat(sprintf("Found %d to be a prime!\n", check_i ) )
   return(c(check_i))
 }
 
